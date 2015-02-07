8db86064c3895697bc43421ae3b24a0ea2f22887
swarna <swarnasras@yahoo.com>
2015-02-07 15:52:22 +0530

    added comments and made minor correction in Plot4.R file

--- Plot3.R
+++ Plot3.R
@@ -1,9 +1,14 @@
+#reads file
 library("data.table")
 file=fread("household_power_consumption.txt")
 file1=as.data.frame(file)
 rm(file)
+
+#checks if date is 1st or 2nd feb 2007
 data=file1[file1$Date %in% c("1/2/2007","2/2/2007"),]
 rm(file1)
+
+#converts data types
 data$Date=as.Date(data$Date,format="%d/%m/%Y")
 data$Global_active_power=as.numeric(data$Global_active_power)
 data$Global_reactive_power=as.numeric(data$Global_reactive_power)
@@ -14,6 +19,8 @@ data$Sub_metering_2=as.numeric(data$Sub_metering_2)
 data$Sub_metering_3=as.numeric(data$Sub_metering_3)
 
 data$Time=as.POSIXct(paste(data$Date,data$Time),format="%Y-%m-%d %H:%M:%S")
+
+#plots the chart
 png(filename="plot3.png", width=504, height=504)
 par(bg = "transparent")
 plot(data$Time,data$Sub_metering_1, xlab="", ylab="Energy sub metering",type="n")
